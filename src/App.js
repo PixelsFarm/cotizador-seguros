119fe61e1d15e6a61cc350e535c5c53a199cc742
hector <h.pascual@ondho.com>
2020-11-14 23:43:54 +0100

    Crear formulario selección datosd

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
+import Formulario from "./components/Formulario";
 import styled from "@emotion/styled";
 
 const Contenedor = styled.div`
@@ -17,7 +18,7 @@ function App() {
 		<Contenedor>
 			<Header titulo="Cotizador de seguros" />
             <ContenedorFormulario>
-                
+                <Formulario />
             </ContenedorFormulario>
 		</Contenedor>
 	);
